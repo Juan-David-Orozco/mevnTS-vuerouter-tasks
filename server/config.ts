0fc2eb6dde5ab5cdc649372fd4ed2d349226c363
Juan-David-Orozco <ing.ele2018@gmail.com>
2022-10-19 12:33:57 -0500

    config server dev

--- server/config.ts
+++ server/config.ts
@@ -1,5 +1,5 @@
 import { config } from "dotenv";
 config();
 
-export const PORT = process.env.PORT
-export const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mevn-db1'
+export const PORT = process.env.PORT || 4000
+export const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mevn-db'
